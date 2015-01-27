4c07a2d4daadc0c5964ac200689c5adba4992260
Maximiliano Fierro <elmasse@gmail.com>
2015-01-27 20:57:19 -0300

    More tests. Added scope and advice as string.

--- src/Advisable.js
+++ src/Advisable.js
@@ -2,43 +2,77 @@
 
 var cocktail = require('cocktail');
 
+function resolvedAdvice(advice, scope){
+    if (typeof advice === 'string') {
+        advice = scope[advice];
+    }
+    return advice;
+}
+
 cocktail.mix({
     '@exports': module,
     '@as'     : 'class',
 
-    after: function (method, advice){
+    /**
+     * @method after
+     * @param method {String} the method on the current instance to be adviced.
+     * @param advice {String|Function} the method or the name of the method to be executed as advice.
+     * @param scope  {Object} the scope for the advice method. Optional. 
+     */
+    after: function (method, advice, scope){
         var base = this[method],
             ret;
+
+        scope  = (scope === undefined ? this : scope); //scope can be null
+        advice = resolvedAdvice(advice, scope);
 
         if (base) {
             this[method] = function _advicedAfter() {
                 ret = base.apply(this, arguments);
-                advice.apply(this, arguments);
+                advice.apply(scope, arguments);
                 return ret;
             };
         }
     },
 
-    before: function (method, advice){
+    /**
+     * @method before
+     * @param method {String} the method on the current instance to be adviced.
+     * @param advice {String|Function} the method or the name of the method to be executed as advice.
+     * @param scope  {Object} the scope for the advice method. Optional. 
+     */
+    before: function (method, advice, scope){
         var base = this[method];
-
+        
+        scope  = (scope === undefined ? this : scope); //scope can be null
+        advice = resolvedAdvice(advice, scope);
+        
         if (base) {
             this[method] = function _advicedBefore() {
-                advice.apply(this, arguments);
+                advice.apply(scope, arguments);
                 return base.apply(this, arguments);
             };
         }
     },
 
-    around: function (method, advice){
+    /**
+     * @method around
+     * @param method {String} the method on the current instance to be adviced.
+     * @param advice {String|Function} the method or the name of the method to be executed as advice.
+     * @param scope  {Object} the scope for the advice method. Optional. 
+     */
+    around: function (method, advice, scope){
         var base = this[method],
             args;
 
+        scope  = (scope === undefined ? this : scope); //scope can be null
+        advice = resolvedAdvice(advice, scope);
+        
         if (base) {
             this[method] = function _advicedAround() {
                 args = Array.prototype.slice.call(arguments);
                 args.unshift(base);
-                return advice.apply(this, args);
+                return advice.apply(scope, args);
             };
         }
     }
--- test/unit/Advisable.js
+++ test/unit/Advisable.js
@@ -26,7 +26,7 @@ describe('Advisable Trait Unit Test', function (){
 
     describe('Advice after', function () {
 
-        it('should execute the method right after the adviced method', function () {
+        it('should execute the advice right after the adviced method', function () {
             var sut = new AdvicedClass(),
                 afterSpy = sinon.spy();
 
@@ -36,6 +36,68 @@ describe('Advisable Trait Unit Test', function (){
 
             expect(advicedMethod).to.be.calledWith(undefined);
             expect(afterSpy).to.be.calledWith(undefined);
+            expect(afterSpy).to.be.calledOn(sut);
+        });
+
+        it('should execute the advice right after the adviced method on the given scope', function () {
+            var sut = new AdvicedClass(),
+                afterSpy = sinon.spy(),
+                scope = {};
+
+            sut.after('method', afterSpy, scope);
+
+            sut.method();
+
+            expect(advicedMethod).to.be.calledWith(undefined);
+            expect(afterSpy).to.be.calledWith(undefined);
+            expect(afterSpy).to.be.calledOn(scope);
+        });
+
+        it('should execute the advice right after the adviced method on the given scope even if scope is null', function () {
+            var sut = new AdvicedClass(),
+                afterSpy = sinon.spy(),
+                scope = null;
+
+            sut.after('method', afterSpy, scope);
+
+            sut.method();
+
+            expect(advicedMethod).to.be.calledWith(undefined);
+            expect(afterSpy).to.be.calledWith(undefined);
+            expect(afterSpy).to.be.calledOn(scope);
+        });
+
+        it('should execute the advice right after the adviced method on `this` if no scope is defined', function () {
+            var sut = new AdvicedClass(),
+                afterSpy = sinon.spy();
+
+            sut.afterMethod = afterSpy;
+
+            sut.after('method', 'afterMethod');
+
+            sut.method();
+
+            expect(advicedMethod).to.be.calledWith(undefined);
+            expect(afterSpy).to.be.calledWith(undefined);
+            expect(afterSpy).to.be.calledOn(sut);
+        });
+
+        it('should execute the advice right after the adviced method on the given scope', function () {
+            var sut = new AdvicedClass(),
+                afterSpy = sinon.spy(),
+                scope = {
+                    afterMethod: afterSpy
+                };
+
+            sut.afterMethod = afterSpy;
+
+            sut.after('method', 'afterMethod', scope);
+
+            sut.method();
+
+            expect(advicedMethod).to.be.calledWith(undefined);
+            expect(afterSpy).to.be.calledWith(undefined);
+            expect(afterSpy).to.be.calledOn(scope);
         });
 
         it('should execute the method right after the adviced method with the same parameters ', function () {
@@ -50,14 +112,15 @@ describe('Advisable Trait Unit Test', function (){
 
             expect(advicedMethod).to.be.calledWith(param1, param2);
             expect(afterSpy).to.be.calledWith(param1, param2);
+            expect(afterSpy).to.be.calledOn(sut);
             
-        });       
+        });
 
     });
 
     describe('Advice before', function () {
 
-        it('should execute the method right before the adviced method', function () {
+        it('should execute the advice right before the adviced method', function () {
             var sut = new AdvicedClass(),
                 beforeSpy = sinon.spy();
 
@@ -67,10 +130,74 @@ describe('Advisable Trait Unit Test', function (){
 
             expect(beforeSpy).to.be.calledWith(undefined);
             expect(advicedMethod).to.be.calledWith(undefined);
+            expect(beforeSpy).to.be.calledOn(sut);
             
         });
 
-        it('should execute the method right before the adviced method with the same parameters ', function () {
+        it('should execute the advice right before the adviced method with the given scope', function () {
+            var sut = new AdvicedClass(),
+                beforeSpy = sinon.spy(),
+                scope = {};
+
+            sut.before('method', beforeSpy, scope);
+
+            sut.method();
+
+            expect(beforeSpy).to.be.calledWith(undefined);
+            expect(advicedMethod).to.be.calledWith(undefined);
+            expect(beforeSpy).to.be.calledOn(scope);
+            
+        });
+
+        it('should execute the advice right before the adviced method with the given scope even if scope is null', function () {
+            var sut = new AdvicedClass(),
+                beforeSpy = sinon.spy(),
+                scope = null;
+
+            sut.before('method', beforeSpy, scope);
+
+            sut.method();
+
+            expect(beforeSpy).to.be.calledWith(undefined);
+            expect(advicedMethod).to.be.calledWith(undefined);
+            expect(beforeSpy).to.be.calledOn(scope);
+            
+        });
+
+        it('should execute the advice right before the adviced method on `this` if no scope is defined', function () {
+            var sut = new AdvicedClass(),
+                beforeSpy = sinon.spy();
+
+            sut.beforeMethod = beforeSpy;
+
+            sut.before('method', 'beforeMethod');
+
+            sut.method();
+
+            expect(advicedMethod).to.be.calledWith(undefined);
+            expect(beforeSpy).to.be.calledWith(undefined);
+            expect(beforeSpy).to.be.calledOn(sut);
+        });
+
+        it('should execute the advice right before the adviced method on the given scope', function () {
+            var sut = new AdvicedClass(),
+                beforeSpy = sinon.spy(),
+                scope = {
+                    beforeMethod: beforeSpy
+                };
+
+            sut.beforeMethod = beforeSpy;
+
+            sut.after('method', 'beforeMethod', scope);
+
+            sut.method();
+
+            expect(advicedMethod).to.be.calledWith(undefined);
+            expect(beforeSpy).to.be.calledWith(undefined);
+            expect(beforeSpy).to.be.calledOn(scope);
+        });
+
+        it('should execute the advice right before the adviced method with the same parameters ', function () {
             var sut = new AdvicedClass(),
                 beforeSpy = sinon.spy(),
                 param1 = '1',
@@ -82,13 +209,14 @@ describe('Advisable Trait Unit Test', function (){
 
             expect(beforeSpy).to.be.calledWith(param1, param2);
             expect(advicedMethod).to.be.calledWith(param1, param2);
+            expect(beforeSpy).to.be.calledOn(sut);
             
         });        
     });    
 
     describe('Advice around', function () {
 
-        it('should execute the method around the adviced method', function () {
+        it('should execute the advice around the adviced method', function () {
             var sut = new AdvicedClass(),
                 aroundSpy = sinon.spy();
             
@@ -96,11 +224,72 @@ describe('Advisable Trait Unit Test', function (){
             sut.method();
 
             expect(aroundSpy).to.be.calledWith(advicedMethod);
+            expect(aroundSpy).to.be.calledOn(sut);
             expect(advicedMethod).to.not.be.called;
 
         });
 
-        it('should execute the method around the adviced method with the same parameters', function () {
+        it('should execute the advice around the adviced method with the given scope', function () {
+            var sut = new AdvicedClass(),
+                aroundSpy = sinon.spy(),
+                scope = {};
+            
+            sut.around('method', aroundSpy, scope);
+            sut.method();
+
+            expect(aroundSpy).to.be.calledWith(advicedMethod);
+            expect(aroundSpy).to.be.calledOn(scope);
+            expect(advicedMethod).to.not.be.called;
+
+        });
+
+        it('should execute the advice around the adviced method on `this`', function () {
+            var sut = new AdvicedClass(),
+                aroundSpy = sinon.spy();
+            
+            sut.aroundMethod = aroundSpy;
+
+            sut.around('method', 'aroundMethod');
+            sut.method();
+
+            expect(aroundSpy).to.be.calledWith(advicedMethod);
+            expect(aroundSpy).to.be.calledOn(sut);
+            expect(advicedMethod).to.not.be.called;
+
+        });
+
+        it('should execute the advice around the adviced method on the given scope', function () {
+            var sut = new AdvicedClass(),
+                aroundSpy = sinon.spy(),
+                scope = {
+                    aroundMethod: aroundSpy
+                };
+            
+
+            sut.around('method', 'aroundMethod', scope);
+            sut.method();
+
+            expect(aroundSpy).to.be.calledWith(advicedMethod);
+            expect(aroundSpy).to.be.calledOn(scope);
+            expect(advicedMethod).to.not.be.called;
+
+        });        
+
+        it('should execute the advice around the adviced method with the given scope even if scope is null', function () {
+            var sut = new AdvicedClass(),
+                aroundSpy = sinon.spy(),
+                scope = null;
+            
+            sut.around('method', aroundSpy, scope);
+            sut.method();
+
+            expect(aroundSpy).to.be.calledWith(advicedMethod);
+            expect(aroundSpy).to.be.calledOn(scope);
+            expect(advicedMethod).to.not.be.called;
+
+        });
+
+        it('should execute the advice around the adviced method with the same parameters', function () {
             var sut = new AdvicedClass(),
                 aroundSpy = sinon.spy(),
                 param1 = '1',
@@ -111,11 +300,12 @@ describe('Advisable Trait Unit Test', function (){
             sut.method(param1, param2);
 
             expect(aroundSpy).to.be.calledWith(advicedMethod, param1, param2);
+            expect(aroundSpy).to.be.calledOn(sut);
             expect(advicedMethod).to.not.be.called;
 
         });
 
-        it('should execute the adviced method if it is called from the around method', function () {
+        it('should execute the adviced method if it is called from the around advice', function () {
             var sut = new AdvicedClass(),
                 aroundFn = function (m) {m.apply(this);},
                 aroundSpy = sinon.spy(aroundFn);
@@ -124,6 +314,7 @@ describe('Advisable Trait Unit Test', function (){
             sut.method();
 
             expect(aroundSpy).to.be.calledWith(advicedMethod);
+            expect(aroundSpy).to.be.calledOn(sut);
             expect(advicedMethod).to.be.called;
 
         });
